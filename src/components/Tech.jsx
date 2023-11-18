728f310b05c645912115108161d83c7c9d4b41ef
“Mohammed <mosanaved@gmail.com>
2023-11-18 11:25:21 +0530

    tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,15 +7,15 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className="mt-[150px] sm:mt-[10px] flex flex-row flex-wrap justify-center gap-10">
+    <div className="mt-[100px] sm:mt-[10px] flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          {/* <img
+        <div className="w-[75px] h-[75px]" key={technology.name}>
+          <img
             src={technology.icon}
             alt={technology.name}
-            className={`bg-inherit hover:bg-zinc-100 duration-300 transition-colors rounded-full p-2`}
-          /> */}
-          <BallCanvas icon={technology.icon} />
+            className={`bg-zinc-100 duration-300 shadow-white transition-colors rounded-full p-1`}
+          />
+          {/* <BallCanvas icon={technology.icon} /> */}
         </div>
       ))}
     </div>
