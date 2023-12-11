656aa60a87110d0e24ccbd78019f1c522f2d79fb
“Mohammed <mosanaved@gmail.com>
2023-12-11 18:23:34 +0530

    working in pc but not in mobile device WorkSection

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -81,22 +81,26 @@ const ProjectCard = ({
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      {/* <motion.div variants={textVariant()}> */}
+      <div>
         <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-      </motion.div>
+      </div>
+      {/* </motion.div> */}
 
       <div className="w-full flex">
-        <motion.p
+        {/* <motion.p
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
-        >
+        > */}
+        <p className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
           links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
-        </motion.p>
+        </p>
+        {/* </motion.p> */}
       </div>
 
       <div className="mt-20 flex flex-wrap gap-7">
